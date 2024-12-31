8ee0e31b429f2435eafa162c459a9694de7c3fcc
Ritika Shah <ritikashah842@gmail.com>
2024-12-31 15:29:51 -0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+# string
+name = "Isha"
+print(type(name))
+#integer
+roll_number = 17
+
+#floating number
+percentage = 95.8
+
+#boolen
+is_student = True
+
+print(name, roll_number, percentage, is_student)
+
+percentage = 94
+
+print(name, roll_number, percentage, is_student)
+
+print = ("My name is" + "and my roll number is", roll_number)
+print = ("I scored", percentage, "% in my final exams. I am a student is", is_student)
